e88a3cf19a7f874a998eb55421f858ab84fbc772
Alessia <a.amore9393@gmail.com>
2022-09-15 19:01:47 +0200

    added filtering and cleaned up a bit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 window.addEventListener("DOMContentLoaded", init);
 
 const url = "https://petlatkea.dk/2021/hogwarts/students.json";
-const allStudents = [];
+let allStudents = [];
 const Student = {
   //template
   firstName: "",
@@ -12,16 +12,33 @@ const Student = {
   nickName: "",
   imageFile: "",
   house: "",
+  gender: "",
+  expelled: false,
+  bloodStatus: "",
+  inquisitorialSquad: false,
+};
+const settings = {
+  filterBy: "all",
+  sortBy: "name",
+  sortDir: "asc",
 };
 
 let previousChar;
-let studentHouse;
-let studentFullName;
 
 function init() {
   console.log("ready");
-
+  //event listener for the drop down options
+  addEventsToButtons();
   loadJSON();
+}
+function addEventsToButtons() {
+  document.querySelector("#filter").addEventListener("change", readOptionsValues);
+}
+
+function readOptionsValues(event) {
+  //reads the value of the filter option selected
+  const selectedFilterOption = event.target.value;
+  setFilter(selectedFilterOption);
 }
 
 function loadJSON() {
@@ -34,90 +51,143 @@ function loadJSON() {
 }
 
 function prepareStudentsData(jsonData) {
-  //create object from the student template
-  jsonData.forEach((jsonObject) => {
-    const student = Object.create(Student);
-
-    //variables for easier use
-    studentFullName = jsonObject.fullname.trim();
-    studentHouse = jsonObject.house.trim();
-
-    //conditions for exeptions
-    for (let i = 1; i < studentFullName.lenght; i++) {
-      previousChar = studentFullName[i - 1];
-      if (previousChar === '"' || previousChar === "-") {
-        studentFullName[i].toUpperCase();
-        //makes sure that if the previous character is ", the letter becomes upper case
-      } else {
-        studentFullName[i];
-      }
-    }
-
-    //first name
-    student.firstName = studentFullName.split(" ")[0].charAt(0).toUpperCase() + studentFullName.split(" ")[0].substring(1).toLowerCase();
-
-    //last name
-    student.lastName = studentFullName.split(" ").at(-1).charAt(0).toUpperCase() + studentFullName.split(" ").at(-1).substring(1).toLowerCase();
-
-    //checking for middle name
-    if (studentFullName.indexOf(" ") === studentFullName.lastIndexOf(" ")) {
-      student.middleName = "";
-    } else {
-      student.middleName = studentFullName.substring(studentFullName.indexOf(" ") + 1, studentFullName.lastIndexOf(" "));
-    }
-
-    //middle name
-    student.middleName = student.middleName.charAt(0).toUpperCase() + student.middleName.substring(1).toLowerCase();
-
-    //nickname
-    student.nickName = studentFullName.substring(studentFullName.indexOf('"') + 1, studentFullName.lastIndexOf('"'));
-
-    //if they have a nickname, they don't have a middlename
-    //incorrect but temporary fix for Ernie
-    if (student.nickName != "") {
-      student.middleName = "";
-    } else {
-      student.middleName = student.middleName;
-    }
-
-    //image file
-    if (student.lastName === "Patil") {
-      student.imageFile = `images/${student.lastName.toLowerCase()}_${student.firstName.toLowerCase()}.png`;
-    } else if (student.lastName === "Finch-fletchley") {
-      const finchArray = student.lastName.split("-");
-      student.imageFile = `images/${finchArray[1].toLowerCase()}_${student.firstName.charAt(0).toLowerCase()}.png`;
-      student.lastName = finchArray[1].charAt(0).toUpperCase() + finchArray[1].substring(1).toLowerCase();
-      student.middleName = finchArray[0];
-    } else {
-      student.imageFile = `images/${student.lastName.toLowerCase()}_${student.firstName.charAt(0).toLowerCase()}.png`;
-    }
-
-    // house
-    student.house = studentHouse.charAt(0).toUpperCase() + studentHouse.substring(1).toLowerCase();
-    console.log("student object", student);
-
-    // push object into array
-    allStudents.push(student);
-
-    //displaying the list length at the end of the page
-    document.querySelector(".number_displayed").textContent = allStudents.length;
-  });
-
-  displayStudentsList(allStudents);
+  allStudents = jsonData.map(prepareStudentData);
+  buildStudentsList();
 }
 
-function displayStudentsList(allStudents) {
+function prepareStudentData(jsonObject) {
+  //create object from the student template
+  const student = Object.create(Student);
+  const studentFullNameArray = jsonObject.fullname.trim().split(" ");
+  student.gender = jsonObject.gender;
+  //first name
+  student.firstName = studentFullNameArray[0].charAt(0).toUpperCase() + studentFullNameArray[0].substring(1).toLowerCase();
+  //last name
+  student.lastName = studentFullNameArray.at(-1).charAt(0).toUpperCase() + studentFullNameArray.at(-1).substring(1).toLowerCase();
+  //conditions for exeptions with letter
+  for (let i = 1; i < jsonObject.fullname.lenght; i++) {
+    previousChar = jsonObject.fullname[i - 1];
+    if (previousChar === '"' || previousChar === "-") {
+      jsonObject.fullname[i].toUpperCase();
+      //makes sure that if the previous character is ", the letter becomes upper case
+    } else {
+      jsonObject.fullname[i];
+    }
+  }
+
+  //checking for middle name
+  if (jsonObject.fullname.indexOf(" ") === jsonObject.fullname.lastIndexOf(" ")) {
+    student.middleName = "";
+  } else {
+    student.middleName = jsonObject.fullname.substring(jsonObject.fullname.indexOf(" ") + 1, jsonObject.fullname.lastIndexOf(" "));
+  }
+
+  //middle name
+  student.middleName = student.middleName.charAt(0).toUpperCase() + student.middleName.substring(1).toLowerCase();
+
+  //nickname
+  student.nickName = jsonObject.fullname.substring(jsonObject.fullname.indexOf('"') + 1, jsonObject.fullname.lastIndexOf('"'));
+
+  //if they have a nickname, they don't have a middlename
+  //incorrect but temporary fix for Ernie
+  if (student.nickName != "") {
+    student.middleName = "";
+  } else {
+    student.middleName = student.middleName;
+  }
+
+  //image file
+  if (student.lastName === "Patil") {
+    student.imageFile = `images/${student.lastName.toLowerCase()}_${student.firstName.toLowerCase()}.png`;
+  } else if (student.lastName === "Finch-fletchley") {
+    const finchArray = student.lastName.split("-");
+    student.imageFile = `images/${finchArray[1].toLowerCase()}_${student.firstName.charAt(0).toLowerCase()}.png`;
+    student.lastName = finchArray[1].charAt(0).toUpperCase() + finchArray[1].substring(1).toLowerCase();
+    student.middleName = finchArray[0];
+  } else {
+    student.imageFile = `images/${student.lastName.toLowerCase()}_${student.firstName.charAt(0).toLowerCase()}.png`;
+  }
+
+  // house
+  student.house = jsonObject.house.trim();
+  student.house = student.house.charAt(0).toUpperCase() + student.house.substring(1).toLowerCase();
+  console.log("student object", student);
+
+  return student;
+}
+
+function setFilter(filter) {
+  settings.filterBy = filter;
+  buildStudentsList();
+}
+
+function buildStudentsList() {
+  const currentList = filterList(allStudents);
+  console.log("current list:", currentList);
+
+  displayStudentsList(currentList);
+}
+
+function filterList(filteredList) {
+  if (settings.filterBy === "house_gryffindor") {
+    filteredList = allStudents.filter(isGryffindor);
+  }
+  if (settings.filterBy === "house_ravenclaw") {
+    filteredList = allStudents.filter(isRavenclaw);
+  }
+  if (settings.filterBy === "house_hufflepuff") {
+    filteredList = allStudents.filter(isHufflePuff);
+  }
+  if (settings.filterBy === "house_Slytherin") {
+    filteredList = allStudents.filter(isSlytherin);
+  }
+  if (settings.filterBy === "expelled") {
+    filteredList = allStudents.filter(isExpelled);
+  }
+  if (settings.filterBy === "not_expelled") {
+    filteredList = allStudents.filter(isNotExpelled);
+  }
+  return filteredList;
+}
+
+function isGryffindor(student) {
+  return student.house === "Gryffindor";
+}
+function isRavenclaw(student) {
+  return student.house === "Ravenclaw";
+}
+function isHufflePuff(student) {
+  return student.house === "Hufflepuff";
+}
+function isSlytherin(student) {
+  return student.house === "Slytherin";
+}
+function isExpelled(student) {
+  return student.expelled === true;
+}
+function isNotExpelled(student) {
+  return student.expelled === false;
+}
+
+function displayStudentsList(students) {
   // clear the list
-  //   document.querySelector("#list tbody").innerHTML = "";
-  console.log("arrayStudents", allStudents);
+  document.querySelector("#template_wrapper").innerHTML = " ";
+  console.log("arrayStudents", students);
   // build a new list
-  allStudents.forEach(displayStudent);
+  students.forEach(displayStudent);
+  //displaying the stats info
+  document.querySelector(".number_total_students").textContent = students.length;
+  document.querySelector(".number_displayed").textContent = students.length;
+  document.querySelector(".number_expelled").textContent = students.filter(isExpelled).length;
+  document.querySelector(".number_gryffindor").textContent = students.filter(isGryffindor).length;
+  document.querySelector(".number_ravenclaw").textContent = students.filter(isRavenclaw).length;
+  document.querySelector(".number_hufflepuff").textContent = students.filter(isHufflePuff).length;
+  document.querySelector(".number_slytherin").textContent = students.filter(isSlytherin).length;
 }
 
 function displayStudent(student) {
   // create clone
-  const clone = document.querySelector("template.student_card_template").content.cloneNode(true);
-
+  const clone = document.querySelector(".student_card_template").content.cloneNode(true);
   // set clone data
   clone.querySelector(".name").textContent = student.firstName;
   clone.querySelector(".last_name").textContent = student.lastName;
@@ -125,7 +195,7 @@ function displayStudent(student) {
   clone.querySelector(".student_picture").alt = `${student.firstName} ${student.lastName}`;
 
   // append clone to list
-  document.querySelector(".main_wrapper").appendChild(clone);
+  document.querySelector("#template_wrapper").appendChild(clone);
 }
 
 //for searching - use filtering on the input search field return the array of things that match the search
